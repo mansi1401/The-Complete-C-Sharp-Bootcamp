2add1e4b681b3b37e328bc90ce54fcd7c9f6f8a7
Ashish Sahu <ashsahu9713@gmail.com>
2023-09-24 20:57:04 +0530

    Update

--- Practice For C#/Pass By Value And Pass By Reference (ref And Out Keywords)/Program.cs
+++ Practice For C#/Pass By Value And Pass By Reference (ref And Out Keywords)/Program.cs
@@ -13,12 +13,37 @@ namespace Pass_By_Value_And_Pass_By_Reference__ref_And_Out_Keywords_
             num1 = num1 + 10;
             Console.WriteLine("Value is " + num1);
         }
+
+        public static void PassByReference(ref int num2)
+        {
+            num2 = num2 + 10;
+            Console.WriteLine("Value is " + num2);
+        }
+
+        public static void PassByOut(out int num3)
+        {
+            num3 = 10;
+            Console.WriteLine("Value is " + num3);
+        }
+
         static void Main(string[] args)
         {
+            //Pass By Value
+            Console.WriteLine("Pass By Value");
             int value = 5;
             Program.PassByValue(value);
-
             Console.WriteLine(value);
+
+            //Pass By Reference
+            Console.WriteLine("Pass By Reference");
+            Program.PassByReference(ref value);
+            Console.WriteLine(value);
+
+            //PassByOut
+            Console.WriteLine("Pass By Out");
+            int data;
+            Program.PassByOut(out data);
+            Console.WriteLine(data);
 
             Console.ReadLine();
         }
---
+++ Practice For C#/Var And Dynamic Keyword/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_And_Dynamic_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Var Keyword");
+            var b = 5;
+            Console.WriteLine("\nvalue of b is " + b);
+            Console.WriteLine("Type of b is " + b.GetType());
+
+            var a = "Maan";
+            Console.WriteLine("\nvalue of a is " + a);
+            Console.WriteLine("Type of a is " + a.GetType());
+
+            //var a;
+
+            Console.WriteLine("\nDynamic Keyword");
+
+            dynamic abc = 50;
+            Console.WriteLine("\nvalue of abc is " + abc);
+            Console.WriteLine("Type of abc is " + abc.GetType());
+
+            dynamic xyz;
+            xyz = "Maan";
+            Console.WriteLine("\nvalue of xyz is " + xyz);
+            Console.WriteLine("Type of xyz is " + xyz.GetType());
+
+            dynamic pqr = 10;
+            Console.WriteLine("\nvalue of pqr is " + pqr);
+            pqr = "Ash";
+            Console.WriteLine("\nvalue of pqr is " + pqr);
+            Console.WriteLine("Type of pqr is " + pqr.GetType());
+
+            Console.ReadLine();
+        }
+    }
+}
