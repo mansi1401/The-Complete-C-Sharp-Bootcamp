4f243ee3c13cce592f01ac042ecf8a3cc9bdd51d
Ashish Sahu <ashsahu9713@gmail.com>
2023-09-24 20:26:54 +0530

    Update

---
+++ Practice For C#/Pass By Value And Pass By Reference (ref And Out Keywords)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pass_By_Value_And_Pass_By_Reference__ref_And_Out_Keywords_
+{
+    internal class Program
+    {
+        public static void PassByValue(int num1)
+        {
+            num1 = num1 + 10;
+            Console.WriteLine("Value is " + num1);
+        }
+        static void Main(string[] args)
+        {
+            int value = 5;
+            Program.PassByValue(value);
+
+            Console.WriteLine(value);
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,5 @@ A namespace is used to organize your code and is collection of classes, interfac
 
 Main method is the entry point into your application.
 
+# Pass By Value And Pass By Reference (ref And Out Keywords)
+
